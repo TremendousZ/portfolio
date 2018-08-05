4e05670cf8ad251fe8589a7caae1df39c4fd7239
John Carlisle <jmcarlisle@live.com>
2018-08-05 12:51:56 -0700

    First commit

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+	// ** Start use of strict
+	
+	"use strict";
+
+	// ** Navigation bar collapse on scroll
+
+	$('#navbar-affix').affix({
+	    offset: {
+			top: 100
+		}
+	});
+
+	// ** Smooth scrolling
+
+	$("a").on('click', function(event) {
+
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			event.preventDefault();
+
+			// Store hash
+			var hash = this.hash;
+
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (1000) specifies the number of milliseconds it takes to scroll to the specified area.
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 1000, function(){
+
+				// Add hash (#) to end of the URL when done scrolling 
+				window.location.hash = hash;
+
+		    });
+		}
+	
+	});
+});
