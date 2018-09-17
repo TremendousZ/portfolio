b2b16b0d49de6492f8adb8108e96c89f14c38389
John Carlisle <jmcarlisle@live.com>
2018-09-17 09:30:09 -0700

    fixed php mailer, needed form action and pathing to mailer file

--- php_mailer/mail_handler.php
+++ php_mailer/mail_handler.php
@@ -1,7 +1,6 @@
 <?php
 require_once('email_config.php');
 require('phpmailer/PHPMailer/PHPMailerAutoload.php');
-
 foreach($_POST as $key=>$value){
     $_POST[$key] = htmlentities ( addslashes( $value ));
 }
@@ -46,7 +45,7 @@ if($output['success'] !== null){
 
 
 $mail = new PHPMailer;
-$mail->SMTPDebug = 3;           // Enable verbose debug output. Change to 0 to disable debugging output.
+$mail->SMTPDebug = 0;           // Enable verbose debug output. Change to 0 to disable debugging output.
 
 $mail->isSMTP();                // Set mailer to use SMTP.
 $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers.
